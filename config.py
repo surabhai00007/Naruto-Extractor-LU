27fe11cc41a13c174f67de12940f21812d8b8828
surabhai00007 <surabhai00007@gmail.com>
2025-08-30 07:31:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,18 +1,18 @@
 import os
 
 class Config(object):
-    BOT_TOKEN = "7472777466:AAHdCsYtZpPnoMv67OL5_Hv2yjvy2Nz8jEg"
-    API_ID = 22581733
-    API_HASH = "1db7bdcf908100cc641c6a5276765c3d"
-    ADMIN = '6530997270'.split(',')
+    BOT_TOKEN = "7662957579:AAFPfWWvp40cLNgi85AOsAaIUDtiOpc6Bhk"
+    API_ID = 22537641
+    API_HASH = "6c1eee32be959812f0598919209a2105"
+    ADMIN = '6748247898'.split(',')
     ADMIN_ID = [int(id) for id in ADMIN]
     DB_URL = "mongodb+srv://neoayush444:3kuwGf9jKicbDxvT@cluster0.f9vq7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     DB_NAME = "Crack_Jack"
-    TXT_LOG = -1002294532244
-    AUTH_LOG = -1002294532244
-    HIT_LOG = -1002294532244
-    DRM_DUMP = -1002294532244
-    CHANNEL = --1002487777733
+    TXT_LOG = -1002931147506
+    AUTH_LOG = -1002931147506
+    HIT_LOG = -1002931147506
+    DRM_DUMP = -1002931147506
+    CHANNEL = --1002775661427
     CH_URL = "https://t.me/justfortestinggwak"
     OWNER = "https://t.me/mrcraacker"
     THUMB_URL = "https://vault.pictures/media/images/b9/ad/12/b9ad123d95cd49465fcb954d.jpg" #Replace by with your Thumb URL
